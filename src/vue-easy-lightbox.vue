a6ec2458c66f53a3bf70cc470e5a48ad757c1381
XiongAmao <onycat@live.cn>
2019-06-08 17:23:21 +0800

    :bug: fix Bootstarp support #10

--- src/vue-easy-lightbox.vue
+++ src/vue-easy-lightbox.vue
@@ -251,6 +251,7 @@
   }
   .img-swiper {
     position: relative;
+    display: block;
   }
   .modal {
     z-index: 9998;
@@ -259,6 +260,7 @@
     left: 0;
     right: 0;
     bottom: 0;
+    margin: 0;
     background: rgba(0, 0, 0, 0.5);
   }
   .img-wrapper {
