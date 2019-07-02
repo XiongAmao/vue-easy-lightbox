00c1fd8b66e779fb176395c0d318cfdbbe7ee746
XiongAmao <onycat@live.cn>
2019-07-02 18:55:24 +0800

    Configurate babel for rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,79 @@
 import vue from 'rollup-plugin-vue'
-import css from 'rollup-plugin-css-only'
+// import css from 'rollup-plugin-css-only'
 import commonJs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+
+const babelConfig = {
+  babelrc: false,
+  runtimeHelpers: true,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        loose: false,
+        useBuiltIns: 'usage'
+      }
+    ],
+    ['@vue/babel-preset-jsx', {}]
+  ],
+  // plugins: [
+  //   [
+  //     '@babel/plugin-transform-runtime',
+  //     {
+  //       regenerator: false,
+  //       corejs: false,
+  //       helpers: true,
+  //       useESModules: true
+  //     }
+  //   ]
+  // ],
+  exclude: 'node_modules/**' // only transpile our source code
+}
 
 export default [
   {
     input: 'src/index.js',
     output: {
       format: 'esm',
-      file: 'test-dist/library.esm.js'
-    },
-    plugins: [
-      commonJs(),
-      // css(),
-      // vue({ css: false })
-      vue()
-    ]
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      format: 'cjs',
-      file: 'test-dist/library.common.js'
-    },
-    plugins: [
-      commonJs(),
-      // css(),
-      // vue({ css: false })
-      vue()
-    ]
-  },
-  {
-    input: 'src/index.js',
-    output: {
-      format: 'umd',
-      file: 'test-dist/library.umd.js',
+      file: 'test-dist/library.esm.js',
       name: 'vue-easy-lightbox'
     },
     plugins: [
-      commonJs(),
-      // css(),
-      // vue({ css: false })
-      vue()
+      vue(),
+      babel(babelConfig),
+      resolve(),
+      commonJs()
     ]
   }
+  // {
+  //   input: 'src/index.js',
+  //   output: {
+  //     format: 'cjs',
+  //     file: 'test-dist/library.common.js'
+  //   },
+  //   plugins: [
+  //     commonJs(),
+  //     // css(),
+  //     // vue({ css: false })
+  //     vue()
+  //   ]
+  // },
+  // {
+  //   input: 'src/index.js',
+  //   output: {
+  //     format: 'umd',
+  //     file: 'test-dist/library.umd.js',
+  //     name: 'vue-easy-lightbox'
+  //   },
+  //   plugins: [
+  //     commonJs(),
+  //     // css(),
+  //     // vue({ css: false })
+  //     vue()
+  //   ]
+  // }
   // {
   //   input: 'src/index.js',
   //   output: {
