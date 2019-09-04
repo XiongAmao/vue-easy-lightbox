3ad9f78b1196f0e88fee585a9bf92533cc733256
XiongAmao <onycat@live.cn>
2019-09-04 22:12:11 +0800

    Add ts dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,61 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es6: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': 'off',
-    'space-before-function-paren': 'off'
+    semi: ['error', 'never'],
+    indent: 'off',
+    'space-before-function-paren': 'off',
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: true,
+        ignores: []
+      }
+    ],
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': [
+      'error',
+      'always',
+      { arraysInObjects: true, objectsInObjects: true }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'vue/multiline-html-element-content-newline': 'off'
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    '@vue/standard',
+    '@vue/typescript'
+  ]
 }
