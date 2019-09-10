a112d85a6781b9b3c5b353418de8725f10c95156
XiongAmao <onycat@live.cn>
2019-09-10 22:28:58 +0800

    Only export default

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 export function install (vue: typeof Vue): void
-export declare class VueEasyLightbox extends Vue {
+declare class VueEasyLightbox extends Vue {
   /**
    * Image's Url
    * @requires
