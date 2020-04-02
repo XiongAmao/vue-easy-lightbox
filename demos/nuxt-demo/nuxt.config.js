540b044594cae5be0f5f8098288ec461c4faa7fc
XiongAmao <onycat@live.cn>
2020-04-02 23:59:11 +0800

    chore: add nuxt.js demo

---
+++ demos/nuxt-demo/nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  mode: 'universal',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  plugins: [{ src: '~/plugins/vue-easy-lightbox.js', mode: 'client' }]
+}
---
+++ demos/nuxt-demo/plugins/vue-easy-lightbox.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import VueEasyLightbox from 'vue-easy-lightbox'
+
+Vue.use(VueEasyLightbox)
