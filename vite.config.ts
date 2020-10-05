651b65506138c6db794cdaa7e453433c122bd98a
XiongAmao <onycat@live.cn>
2020-10-05 11:53:52 +0800

    chore: config vite root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { UserConfig } from 'vite'
 import path from 'path'
 
 const config: UserConfig = {
-  root: path.resolve(process.cwd(), './example'),
   outDir: path.resolve(process.cwd(), './dist-gh-page')
 }
 
