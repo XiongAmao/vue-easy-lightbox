eae7f1c404ca1ba8ebd59c9e9dd45ff1ef47e642
XiongAmao <onycat@live.cn>
2021-08-19 23:15:12 +0800

    chore: docs script add init branch name

--- bin/deploy-docs.sh
+++ bin/deploy-docs.sh
@@ -9,10 +9,10 @@ yarn build:docs
 # navigate into the build output directory
 cd docs/docs/.vuepress/dist
 
-git init
+git init -b main
 git add -A
 git commit -m 'update gh-pages'
-git push -f git@github.com:XiongAmao/vue-easy-lightbox.git master:gh-pages
+git push -f git@github.com:XiongAmao/vue-easy-lightbox.git main:gh-pages
 
 cd -
 
