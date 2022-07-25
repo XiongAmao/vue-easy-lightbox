05327a18a85751e5e27de343db4c89f776eb321e
XiongAmao <onycat@live.cn>
2022-07-25 18:55:22 +0800

    chore: config d.ts generator

--- build-scripts/gen-types.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const fs = require('fs-extra')
-const path = require('path')
-
-const resolveTypesPath = (...paths) => path.resolve(__dirname, '../types', ...paths)
-
-// remove useless types
-fs.rm(resolveTypesPath('./dev-entry'), { recursive: true })
-fs.rm(resolveTypesPath('./index.umd.d.ts'))
