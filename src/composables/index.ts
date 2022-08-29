98af1a004c861066469adf091ad83c82336798bc
XiongAmao <onycat@live.cn>
2022-08-29 12:54:52 +0800

    fix(composables): show() accept wrong param when use it in vue template #104
    When a method is passed to the vue `<template><div @click="method"></div></template>`
    
    method is wrapped as `@click="method(MouseEvent)"`

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { isRef, Ref, ref } from 'vue'
 import type { PropsImgs } from '../types'
 
 export interface UseEasyLightboxOptions {
@@ -24,8 +24,16 @@ export const useEasyLightbox = (
   const indexRef = ref(initIndex)
   const visibleRef = ref(false)
 
-  const show = (index = indexRef.value) => {
-    changeIndex(index)
+  const show = (index: Ref<number> | number | Event = indexRef.value) => {
+    if (typeof index === 'number') {
+      changeIndex(index)
+    } else if (isRef(index) && typeof index.value === 'number') {
+      changeIndex(index.value)
+    } else if (index instanceof Event) {
+      // when pass `show()` directly to `@click` in vue template
+      // e.g. @click="show"
+      changeIndex(indexRef.value)
+    }
     visibleRef.value = true
   }
   const changeIndex = (index = indexRef.value) => {
---
+++ src/dev-entry/AppScriptSetup.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <button @click="showSingle">Show single picture.</button>
+    <button @click="showMultiple">Show a group of pictures.</button>
+    <br />
+    <button @click="test">test1</button>
+    <button @click="test2">test2</button>
+    <button @click="show">test show</button>
+
+    <vue-easy-lightbox
+      :visible="visibleRef"
+      :imgs="imgsRef"
+      :index="indexRef"
+      @hide="onHide"
+      @on-index-change="onIndexChange"
+    ></vue-easy-lightbox>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { isReactive, ref } from 'vue'
+import { useEasyLightbox } from '../composables'
+
+const imgList = ref([
+  'http://via.placeholder.com/350x150',
+  'http://via.placeholder.com/250x150'
+])
+
+const { visibleRef, indexRef, imgsRef, show, changeIndex, onHide } =
+  useEasyLightbox({
+    imgs: imgList.value,
+    initIndex: 0
+  })
+
+const test = () => {
+  console.log(isReactive(imgList.value))
+  console.log(imgList.value)
+  show()
+}
+const test2 = () => {
+  imgList.value.push('http://via.placeholder.com/250x150')
+  show()
+}
+
+const showSingle = () => {
+  imgsRef.value = 'http://it-does-not-matter.png/'
+  show()
+}
+const showMultiple = () => {
+  imgsRef.value = [
+    'http://via.placeholder.com/350x150',
+    'http://via.placeholder.com/350x150'
+  ]
+  changeIndex()
+  show()
+}
+const showImg = () => show()
+
+const onIndexChange = (old: number, newN: number) => {
+  console.log(old, newN)
+}
+</script>
