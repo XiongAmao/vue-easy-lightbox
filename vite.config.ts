2e740517ff3df2d29441d8d01376f68fc3e3fed4
XiongAmao <onycat@live.cn>
2023-02-16 17:30:13 +0800

    chore: update ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 export default defineConfig({
   server: {
     port: 8888
+  },
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+    }
   },
   test: {
     globals: true,
